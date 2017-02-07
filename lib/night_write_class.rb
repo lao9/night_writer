ddb5883f4723bf85d5581a3601d27bc21cc6fc50
marcoverzero <marcoverzero@gmail.com>
2017-02-07 16:44:29 -0700

    more outputfiles and tests

--- lib/night_write_class.rb
+++ lib/night_write_class.rb
@@ -3,70 +3,75 @@ require 'pry'
 
 class NightWrite
   attr_reader :file_input
-  attr_accessor :number_bool, :line_1, :line_2, :line_3, :english_array
-  def initialize(file_input)
+  attr_accessor :number_bool, :line_1, :line_2, :line_3, :english_array, :braille_file
+  def initialize(file_input, braille_file)
     @number_bool = false
     @english_array = []
     @line_1 = ""
     @line_2 = ""
     @line_3 = ""
-    @file_input = file_input
+    @file_input = file_input.chomp
+    @braille_file = braille_file
   end
 
     def message_string_split_to_array
       @english_array = @file_input.split(//)
+      @english_array.each do |character|
+        character_orientation(character)
+      end
     end
 
-    def character_orientation
-      @english_array.each do |character|
-        if letters_to_numbers.key(character)!= nil
-          if @number_bool == false
-            translation_builder(alphabet_hash["number"])
-          end
-          translation_builder(alphabet_hash[letters_to_numbers.key(character)])
+    def character_orientation(character)
+      if letters_to_numbers.key(character)!= nil
+        if @number_bool == false
           @number_bool = true
-        elsif character == character.upcase
-          translation_builder(alphabet_hash["capital"])
-          translation_builder(alphabet_hash[character.downcase])
-        elsif character == " "
-            @number_bool = false
-            translation_builder(alphabet_hash[character])
+          translation_builder(alphabet_hash["number"])
+          translation_builder(alphabet_hash[letters_to_numbers.key(character)])
+          return alphabet_hash["number"], alphabet_hash[letters_to_numbers.key(character)]
         else
-            translation_builder(alphabet_hash[character])
+          @number_bool = true
+          translation_builder(alphabet_hash[letters_to_numbers.key(character)])
+          return alphabet_hash[letters_to_numbers.key(character)]
         end
+      elsif character == " "
+          @number_bool = false
+          translation_builder(alphabet_hash[character])
+          return alphabet_hash[character]
+      elsif character == character.upcase
+        translation_builder(alphabet_hash["capital"])
+        translation_builder(alphabet_hash[character.downcase])
+        return alphabet_hash["capital"], alphabet_hash[character.downcase]
+      else
+          translation_builder(alphabet_hash[character])
+          return alphabet_hash[character]
       end
-    write_braille_to_file
-
     end
 
     def translation_builder(braille_array)
         @line_1 += braille_array[0]
         @line_2 += braille_array[1]
         @line_3 += braille_array[2]
-        if @line_1.lenght >= 80
+        if @line_1.length >= 80
             write_braille_to_file
+          # binding.pry
         end
     end
 
 
     def write_braille_to_file
-      # puts braille_file(@Line_1)
-      # puts braille_file(@line_2)
-      # puts braille_file(@line_3)
-      #
+      braille_file.puts(@line_1)
+      braille_file.puts(@line_2)
+      braille_file.puts(@line_3)
 
-
-
-
-
+      @line_1 = ""
+      @line_2 = ""
+      @line_3 = ""
     #Once our each method is complete, we should add the final lines to our program using the “write braille to file” method since in the translation builder, they’re only added once the character limit reaches 80
     end
 
-
-
+    def write_final_lines
+      write_braille_to_file
+    end
 
 # binding.pry
 end
-
-
-@Hash = {crazy hash}
